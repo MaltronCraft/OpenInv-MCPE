84d9ec37276a0251f6e5b8eef07a04fcd3fad2b7
NycuRO <nghergu@yahoo.com>
2017-01-03 21:30:48 +0200

    Update OpenInv.java

--- src/main/OpenInv/OpenInv.java
+++ src/main/OpenInv/OpenInv.java
@@ -1,5 +1,13 @@
 package OpenInv;
 
+import cn.nukkit.block.Block;
+import cn.nukkit.nbt.NBTIO;
+import cn.nukkit.nbt.tag.*;
+import cn.nukkit.item.Item;
+import cn.nukkit.level.Level;
+import cn.nukkit.level.Position;
+import cn.nukkit.math.Vector3;
+import cn.nukkit.tile.Chest;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.TextFormat;
 import cn.nukkit.command.Command;
@@ -28,15 +36,29 @@ public class OpenInv extends PluginBase {
         this.getLogger().info(TextFormat.DARK_RED + "EChest disabled!");
     }
     
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        switch (command.getName()) {
-            case "echest":
-		Player player = (Player) sender;
-		player.addWindow(player.getEnderChestInventory());
-		return true;
-            default:
-		return false;
-	}
+    public void sendEnderChestInventory(Player player){
+	    int block = Block.get(54);
+	    player.getLevel().setBlock(new Vector3(player.x, player.y - 2, player.z), block, true, true);
+	    nbt = new CompoundTag("", [
+		    new ListTag("Items", []),
+		    new StringTag("id", BlockEntity.ENDER_CHEST),
+		    new IntTag("x", floor(player.x)),
+		    new IntTag("y", floor(player.y) - 2),
+		    new IntTag("z", floor(player.z))
+	    ]);
+	    nbt.Items.setTagType(NBTIO.TAG_Compound);
+	    int tile = BlockEntity.createBlockEntity("Ender Chest", player.getLevel().getChunk(player.getX() >> 4, player.getZ() >> 4), nbt);
+	    player.addWindow(tile.getInventory());
+  }
+
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    if (sender instanceof Player) {
+      switch (command.getName()) {
+	case "addwindow":
+          sender.sendMessage("Added chest window!");
+          this.sendEnderChestInventory(sender);
+        break;
+      }
     }
+  }
 }
